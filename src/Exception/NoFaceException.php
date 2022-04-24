0bdfd748c066b6c8aef644baee9c31ca8281dc0e
denghao <denghao@vdinsight.cn>
2022-04-24 20:34:31 +0800

    ..

---
+++ src/Exception/NoFaceException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Throws exception if face was not detected in `faceDetect` call.
+ */
+
+namespace anran\Exception;
+
+use Exception;
+
+class NoFaceException extends Exception {
+
+}
